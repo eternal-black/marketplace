2a2e1f4fb30f6b3934b85437c16368f385122445
proxier <evgeny.yanul@gmail.com>
2018-05-27 13:48:30 +0300

    update sql

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -30,24 +30,21 @@ CREATE TABLE IF NOT EXISTS `company` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE IF NOT EXISTS `order` (
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `address_id` int(11) NOT NULL,
-  `buying_date` date DEFAULT NULL,
-  `delivery_date` date DEFAULT NULL,
+  `login` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  `photo` varchar(45) DEFAULT NULL,
+  `cash` int(11) NOT NULL DEFAULT '0',
+  `rating` int(11) NOT NULL DEFAULT '0',
+  `role` enum('admin','user') NOT NULL DEFAULT 'user',
   `status` enum('active','inactive') NOT NULL DEFAULT 'active',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `product_id` (`product_id`),
-  KEY `address_id` (`address_id`),
-  CONSTRAINT `FK_order_address` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`),
-  CONSTRAINT `FK_order_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
-  CONSTRAINT `FK_order_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE IF NOT EXISTS `product` (
@@ -72,18 +69,21 @@ CREATE TABLE IF NOT EXISTS `product` (
   CONSTRAINT `FK_product_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE IF NOT EXISTS `order` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `firstname` varchar(45) NOT NULL,
-  `lastname` varchar(45) NOT NULL,
-  `photo` varchar(45) DEFAULT NULL,
-  `cash` int(11) NOT NULL DEFAULT '0',
-  `rating` int(11) NOT NULL DEFAULT '0',
-  `role` enum('admin','user') NOT NULL DEFAULT 'user',
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `address_id` int(11) NOT NULL,
+  `buying_date` date DEFAULT NULL,
+  `delivery_date` date DEFAULT NULL,
   `status` enum('active','inactive') NOT NULL DEFAULT 'active',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `id` (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `product_id` (`product_id`),
+  KEY `address_id` (`address_id`),
+  CONSTRAINT `FK_order_address` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`),
+  CONSTRAINT `FK_order_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  CONSTRAINT `FK_order_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
--- src/main/resources/sql/insert.sql
+++ src/main/resources/sql/insert.sql
@@ -12,18 +12,18 @@ INSERT INTO `company` (`id`, `name`) VALUES
 	(1, 'Microsoft'),
 	(2, 'Oracle');
 	
-DELETE FROM `order`;
-INSERT INTO `order` (`id`, `user_id`, `product_id`, `address_id`, `buying_date`, `delivery_date`, `status`) VALUES
-	(1, 3, 1, 1, '2018-05-27', '2018-05-27', 'inactive'),
-	(3, 3, 1, 1, '2018-05-27', NULL, 'active'),
-	(4, 3, 1, 1, '2018-05-27', NULL, 'active');
-	
-DELETE FROM `product`;
-INSERT INTO `product` (`id`, `user_id`, `category_id`, `company_id`, `name`, `description`, `cost`, `discount`, `count`, `rating`, `status`) VALUES
-	(1, 2, 1, 1, 'os', 'win', 100, 0, 1, 0, 'active');
-	
 DELETE FROM `user`;
 INSERT INTO `user` (`id`, `login`, `password`, `firstname`, `lastname`, `photo`, `cash`, `rating`, `role`, `status`) VALUES
 	(1, 'rel', '123', 'rel', 'mayer', NULL, 0, 0, 'admin', 'active'),
 	(2, 'lil', '123', 'lil', 'torn', NULL, 0, 0, 'user', 'active'),
 	(3, 'mel', '123', 'melissa', 'tox', NULL, 0, 0, 'user', 'inactive');
+		
+DELETE FROM `product`;
+INSERT INTO `product` (`id`, `user_id`, `category_id`, `company_id`, `name`, `description`, `cost`, `discount`, `count`, `rating`, `status`) VALUES
+	(1, 2, 1, 1, 'os', 'win', 100, 0, 1, 0, 'active');
+	
+DELETE FROM `order`;
+INSERT INTO `order` (`id`, `user_id`, `product_id`, `address_id`, `buying_date`, `delivery_date`, `status`) VALUES
+	(1, 3, 1, 1, '2018-05-27', '2018-05-27', 'inactive'),
+	(3, 3, 1, 1, '2018-05-27', NULL, 'active'),
+	(4, 3, 1, 1, '2018-05-27', NULL, 'active');
